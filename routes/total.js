f0189fd43b3fd3e14cd6227a7b0aa38a259eec88
Sree Harsha <141744505+Harshasree23@users.noreply.github.com>
2025-01-06 11:57:40 +0530

    Added total route to get all marks

---
+++ routes/total.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { handleTotalGet } = require("../controllers/total");
+
+const TotalRoute = express.Router();
+
+TotalRoute.get("/find", handleTotalGet);
+
+
+module.exports = {
+    TotalRoute,
+}
